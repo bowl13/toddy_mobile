1c13f5bcae27ba02836efa9b6fb6e20e0f66c8d9
bowl13 <mauro@bowl.cl>
2013-07-24 17:40:32 -0400

    update 24/07

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
+        <title>Toddy</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
+        <link href="favicon.ico" rel="icon" type="image/x-icon" />
 
         <!--meta facebook-->
-        <meta property="fb:app_id" content="178779322280867"/>
         <meta property="og:url" content="http://fb.bowl.cl/fbprub/"/>
         <meta property="og:title" content="TITULO PROYECTO"/>
         <meta property="og:description" content="DESCRIPCION APP FACEBOOK"/>
@@ -29,7 +29,7 @@
 
         <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
     </head>
-    <body>
+    <body id="bg_all">
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
@@ -40,10 +40,67 @@
 
         <div id="wrap">
 
-            <div class="container-fluid">
+            <div class="container-fluid" style="max-width:940px;margin:0 auto;">
 
+                <!--NAV-->
+                <header class="row-fluid">
+
+                    <div style="position:relative;z-index:999;">
+                        
+                        <div class="span3">
+                        
+                            <img src="img/logo_toddy.png" alt=""/>
+
+                        </div>
+                        <nav class="span6 nav_prin">
+
+                            <ul class="unstyled nav nav-pills">
+                                <li class="pull-left"><a href="">productos</a></li>
+                                <li class="divider-vertica"></li>
+                                <li class="pull-left"><a href="">juegos</a></li>
+                                <li class="divider-vertical"></li>
+                                <li class="pull-left"><a href="">comic</a></li>
+                            </ul>
+
+                        </nav>
+
+                        <div class="span3 text-center">
+                            
+                            Siguenos en
+
+                            <ul class="unstyled nav_soci">
+                                <li class="pull-left"><img src="img/toddy_tw.gif" alt=""/></li>
+                                <li class="pull-left"><img src="img/toddy_fb.gif" alt=""/></li>
+                                <li class="pull-left"><img src="img/toddy_yt.gif" alt=""/></li>
+                            </ul>
+
+                        </div>
+
+                    </div>
+                    
+                    <img class="bg_nav" src="img/bg_nav.png" alt=""/>
+                </header>
+
+                <!--SLIDE-->
                 <div class="row-fluid">
+                    <div class="span12">
+                        
+                        <div id="promos" class="carousel slide">
+                            
+                            <div class="carousel-inner text-center">
+                                <div class="active item"><a href=""><img src="img/destacados/banner001-trans.png" alt=""/></a></div>
+                                <div class="item"><a href=""><img src="img/destacados/banner01-trans.png" alt=""/></a></div>
+                                <div class="item"><a href=""><img src="img/destacados/banner02-trans.png" alt=""></a></div>
+                                <div class="item"><a href=""><img src="img/destacados/banner03-trans.png" alt=""></a></div>
+                                <div class="item"><a href=""><img src="img/destacados/banner04-trans.png" alt=""></a></div>
+                            </div>
 
+                            <a class="carousel-control left" href="#promos" data-slide="prev"></a>
+                            <a class="carousel-control right" href="#promos" data-slide="next"></a>
+
+                        </div>
+
+                    </div>
                 </div>
 
             </div> <!-- /container -->
@@ -54,9 +111,36 @@
         
         <footer class="footer-fix container-fluid">
             
+            <div class="row-fluid"  style="max-width:940px;margin:0 auto;">
+
+                <div class="span12">
+
+                    <div class="span3 pull-left">
+                        
+                        <img src="img/pro_01.png" alt=""/>
+
+                    </div>
+                    <div class="span3 pull-left">
+                        
+                        <img src="img/pro_02.png" alt=""/>
+
+                    </div>
+                    <div class="span3 pull-left">
+
+                        <img src="img/pro_03.png" alt=""/>
+
+                    </div>
+                    <div class="span3 pull-left">
+                        <div id="ticker"></div>
+                    </div>
+
+                </div>
+
+            </div>
+
             <div class="row-fluid">
 
-                <p class="span12">&copy; Company 2012</p>
+                <p class="span12 text-center" style="font-size:10px;">® Copyright PepsiCo 2013 - Todos los derechos reservados.</p>
 
             </div>
                 
@@ -70,6 +154,7 @@
         <script src="js/vendor/bootstrap.min.js"></script>
 
         <script src="js/main.js"></script>
+        <script charset="utf-8" src="twitter/jquery.tweet.js"></script>
 
 		<!--google analytics-->
 
--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
 
+	$("#ticker").tweet({
+		username: "toddy_cl",
+		page: 2,
+		avatar_size: 40,
+		count: 20,
+		loading_text: "cargando ..."
+	}).bind("loaded", function() {
+		var ul = $(this).find(".tweet_list");
+		var ticker = function() {
+		setTimeout(function() {
+			var top = ul.position().top;
+			var h = ul.height();
+			var incr = (h / ul.children().length);
+			var newTop = top - incr;
+			if (h + newTop <= 0) newTop = 0;
+			ul.animate( {top: newTop}, 500 );
+			ticker();
+		}, 5000);
+		};
+	ticker();
+	});
 });
 //LOADING
 $(window).load(function(){
