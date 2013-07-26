f53dfa969abe47a226fc75d95d83737eeeb7fe3a
bowl13 <mauro@bowl.cl>
2013-07-26 12:05:32 -0400

    update 26/07

---
+++ form_exito.php
@@ -0,0 +1,122 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Toddy</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width">
+
+	<meta property="og:title" content="Toddy te lleva a Suiza"/>
+	<meta property="og:image" content="http://www.bowl.cl/clientes/toddy/form_suiza/images/post_img.jpg"/>
+	<meta property="og:description" content="Que todos los vacunos sepan que acabai de postular para ir hacer relaciones exteriores al distrito del chocolate en Suiza!"/>
+
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" href="bootstrap/css/bootstrap-responsive.css">
+
+	<link rel="stylesheet" href="bootstrap/css/master_suiza.css">
+	<link rel="stylesheet" href="css/internas.css">
+	
+	<!--[if lt IE 9]>
+		<script src="bootstrap/js/html5shiv.js"></script>
+	<![endif]-->
+
+	<script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-42206920-1']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
+</head>
+<body>
+	<div class="container-fluid">
+		
+	<!--botonera toddy-->
+		<div id="botonera_int" class="hidden-phone hidden-tablet">
+			<div id="navegacion_int">
+				<div id="mover">
+					<a href="index.html">
+						<img border="0" src="images/logo-int-trans.png" style="filter:progid:DXImageTransform.Microsoft.AlphaImageLoader (src='imgaes/logo-int-trans',sizingMethod='scale');" />
+					</a>
+					<a href="productos.html" id="btn1"></a>
+					<a href="juegos.html" id="btn2"></a>
+					<a href="comics.html" id="btn3"></a>
+					<div id="share">
+						<a href="http://twitter.com/toddy_cl" target="_blank" id="share1"></a>
+						<a href="http://www.facebook.com/ToddyChile" target="_blank" id="share2"></a>
+						<a href="http://www.youtube.com/user/toddychile?feature=watch" target="_blank" id="share3"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row-fluid hidden-desktop" style="background-image:url('images/top-int-trans.png');position:relative;z-index:99;background-repeat:repeat-x;height:80px;">
+			
+			<img border="0" src="images/logo-int-trans.png" style="filter:progid:DXImageTransform.Microsoft.AlphaImageLoader (src='imgaes/logo-int-trans',sizingMethod='scale');" />
+
+		</div>
+
+
+	<div class="row-fluid">
+			
+		<div class="span12 text-center">
+			
+			<img class="headers" src="images/tit_suiza.gif" alt="Gana un viaje Suiza">
+			
+			<div class="row-fluid bg-w">
+				
+				<div class="span12">
+						
+					<div style="margin-bottom:20px;">
+						
+					<img src="images/tit_exito.gif" alt="">
+					<p>Ahora a morderse las pezuñas esperando el sorteo</p>
+					<p>que se realizará el día 29 de Agosto del 2013.</p>
+					<p>Recuerda guardar tu boleta que acredita la compra de Toddy pa’ la leche!</p>
+
+					</div>
+					
+					<div style="position:relative;margin:50px auto 10px auto;width:220px;">
+								
+							<a href="https://www.facebook.com/sharer/sharer.php?u=http://www.toddy.cl/form_2.php" target="_blank" style="float:left;"><img src="images/btn_fb.gif" alt=""></a>
+							<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://goo.gl/UHbY0" data-text="Acabo de postular para ir hacer relaciones exteriores al distrito del chocolate en Suiza!" data-lang="es" data-size="large" data-count="none" data-hashtags="EligeToddy" style="float:left;">Twittear</a>
+							<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+						
+
+					</div>
+					<div class="clearfix"></div>
+					<div class="text-right">
+						
+						<img src="images/toddy_exito.gif" alt="">
+							
+					</div>
+					
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+	</div>
+	
+
+	<footer class="footer-fix container-fluid text-center">® Copyright PepsiCo 2013 - Todos los derechos reservados.</footer>
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+</html>
